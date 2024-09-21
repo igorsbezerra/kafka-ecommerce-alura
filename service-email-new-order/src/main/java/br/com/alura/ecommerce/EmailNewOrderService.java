28e657b7b44723f80948c6035feb06c2d06d0590
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2024-09-21 11:41:29 -0300

    multi-thread services email new order

--- service-email-new-order/src/main/java/br/com/alura/ecommerce/EmailNewOrderService.java
+++ service-email-new-order/src/main/java/br/com/alura/ecommerce/EmailNewOrderService.java
@@ -6,21 +6,15 @@ import java.math.BigDecimal;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class EmailNewOrderService {
+public class EmailNewOrderService implements ConsumerService<Order> {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var emailService = new EmailNewOrderService();
-        try (var service = new KafkaService<>(EmailNewOrderService.class.getSimpleName(),
-                "ECOMMERCE_NEW_ORDER",
-                emailService::parse,
-                Map.of())) {
-            service.run();
-        }
+    public static void main(String[] args) {
+        new ServiceRunner<>(EmailNewOrderService::new).start(1);
     }
 
     private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
 
-    private void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
+    public void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
         System.out.println("------------------------------------------");
         System.out.println("Processing new order, prepare email");
         System.out.println(record.key());
@@ -31,5 +25,15 @@ public class EmailNewOrderService {
         emailDispatcher.send("ECOMMERCE_SEND_EMAIL", order.getEmail(),
                 record.value().getId().continueWith(EmailNewOrderService.class.getSimpleName()),
                 emailCode);
+    }
+
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_NEW_ORDER";
+    }
+
+    @Override
+    public String getConsumerGroup() {
+        return EmailNewOrderService.class.getSimpleName();
     }
 }
